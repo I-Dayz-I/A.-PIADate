efa33b77077870182b96162062a7c84d2374c3b0
Belzico <61699417+Belzico@users.noreply.github.com>
2022-11-28 13:56:03 +0100

    mini

--- classnode.py
+++ classnode.py
@@ -697,7 +697,7 @@ class ProgramNode(ClassNode):
                 #arreglar esto
                 node=self.posibleProductions[head.name]()
                 #resolver el nodo
-                node.build_ast(productionList,indexProduc,self.contex)
+                node.build_ast(productionList,indexProduc,self.context)
                 #agregarlo a los hijos
                 self.ListStatement.append(node)
                 
